07cb24426caace56f0f19b0739b1f565004fcc4f
Pixelsuft <pixelsuft@gmail.com>
2026-05-10 12:38:28 +0700

    wip

--- src/ui.cpp
+++ src/ui.cpp
@@ -8,7 +8,8 @@
 static Frame* flow;
 
 static void set_selected_button(Button* btn) {
-    for (const char* id : { "Button_Add", "Button_Join" }) {
+    for (const char* id :
+         {"Button_Add", "Button_Join", "Button_Trash", "Button_Start", "Button_Stop"}) {
         Button* b = reinterpret_cast<Button*>(root->child_by_id(id));
         b->bg_color.g = btn != b ? 0.f : 0.2f;
         b->refresh_color();
@@ -30,4 +31,19 @@ void construct_ui() {
         ->set_child((new Image("Image_Join"))->set_texture(res->load_texture("join.png")))
         ->set_click_handler([&](Button* self, Container*) { set_selected_button(self); })
         ->set_rect({84.f, 10.f, 64.f, 64.f});
+    // Trash button
+    root->add_child(new Button("Button_Trash"))
+        ->set_child((new Image("Image_Trash"))->set_texture(res->load_texture("trash.png")))
+        ->set_click_handler([&](Button* self, Container*) { set_selected_button(self); })
+        ->set_rect({158.f, 10.f, 64.f, 64.f});
+    // Start button
+    root->add_child(new Button("Button_Start"))
+        ->set_child((new Image("Image_Start"))->set_texture(res->load_texture("start.png")))
+        ->set_click_handler([&](Button* self, Container*) { set_selected_button(self); })
+        ->set_rect({232.f, 10.f, 64.f, 64.f});
+    // Stop button
+    root->add_child(new Button("Button_Stop"))
+        ->set_child((new Image("Image_Stop"))->set_texture(res->load_texture("stop.png")))
+        ->set_click_handler([&](Button* self, Container*) { set_selected_button(self); })
+        ->set_rect({306.f, 10.f, 64.f, 64.f});
 }
